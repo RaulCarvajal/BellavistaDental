7db8809961e9fdb3d3decf5977518010eb57027d
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2017-03-18 21:40:21 -0700

    Pantallas Principales de usuario
    se crearon las 4 pantallas principales

---
+++ php/PrincipalAdministrador.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bienvenido Administrador</title>
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="#"><span></span> Inicio </a></li>
+        <li> <a href="#"><span></span>Pacientes </a> </li>
+        <li> <a href="#"><span></span>Recepcionistas </a></li>
+        <li> <a href="#"><span></span>Dentistas </a></li>
+        <li> <a href="#"><span></span>Cerrar Sesión</a></li>
+    </ul>
+</nav>
+
+</body>
+</html>
---
+++ php/PrincipalRecepcionista.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bienvenido Recepcionista</title>
+</head>
+<body>
+
+</body>
+</html>
