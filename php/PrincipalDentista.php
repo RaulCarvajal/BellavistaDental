fec73ddff05915edf595a407dbd0eb35c17c00c2
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2017-03-18 22:04:16 -0700

    Estructura de las 4 paginas principales

--- php/PrincipalDentista.php
+++ php/PrincipalDentista.php
@@ -4,6 +4,17 @@
     <title>Bienvenido Dentista</title>
 </head>
 <body>
+<h1>Bienvenido Dentista</h1>
+<nav>
+    <ul>
+        <li><a href="#"><span></span> Inicio </a></li>
+        <li> <a href="#"><span></span>Agenda</a> </li>
+        <li> <a href="#"><span></span>Pacientes</a></li>
+        <li> <a href="#"><span></span>Recetas</a></li>
+        <li> <a href="#"><span></span>Inventario</a></li>
+        <li> <a href="#"><span></span>Cerrar Sesión</a></li>
+    </ul>
+</nav>
 
 </body>
 </html>
--- php/PrincipalPaciente.php
+++ php/PrincipalPaciente.php
@@ -4,6 +4,15 @@
     <title>Bienvenido Paciente</title>
 </head>
 <body>
+<h1>Bienvenido Dentista</h1>
+<nav>
+    <ul>
+        <li><a href="#"><span></span> Inicio </a></li>
+        <li> <a href="#"><span></span>Citas</a> </li>
+        <li> <a href="#"><span></span>Recetas</a></li>
+        <li> <a href="#"><span></span>Cerrar Sesión</a></li>
+    </ul>
+</nav>
 
 </body>
 </html>
