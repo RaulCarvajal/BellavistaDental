b1d4ced76f3900678806f6b5edf6870963f002d0
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2017-03-19 13:34:06 -0700

    no aparecian los nuevos iconos

--- php/PrincipalDentista.php
+++ php/PrincipalDentista.php
@@ -2,17 +2,19 @@
 <html>
 <head>
     <title>Bienvenido Dentista</title>
+    <link rel="stylesheet" href="../CSS/menuVerticalDentista.css">
+    <link rel="stylesheet" href="../CSS/styleIcons.css">
 </head>
 <body>
 <h1>Bienvenido Dentista</h1>
 <nav>
     <ul>
-        <li><a href="#"><span></span> Inicio </a></li>
-        <li> <a href="#"><span></span>Agenda</a> </li>
-        <li> <a href="#"><span></span>Pacientes</a></li>
-        <li> <a href="#"><span></span>Recetas</a></li>
-        <li> <a href="#"><span></span>Inventario</a></li>
-        <li> <a href="#"><span></span>Cerrar Sesión</a></li>
+        <li><a href="#"><span><i class="icon icon-home"></i></span> Inicio </a></li>
+        <li> <a href="#"><span><i class="icon icon-address-book"></i></span>Agenda</a> </li>
+        <li> <a href="#"><span><i class="icon icon-user"></i></span>Pacientes</a></li>
+        <li> <a href="#"><span><i class="icon icon-file-text2"></i></span>Recetas</a></li>
+        <li> <a href="#"><span><i class="icon icon-drawer2"></i></span>Inventario</a></li>
+        <li> <a href="#"><span><i class="icon-cancel-circle"></i></span>Cerrar Sesión</a></li>
     </ul>
 </nav>
 
