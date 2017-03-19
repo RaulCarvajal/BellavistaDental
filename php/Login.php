1b908232418189c078e095f7eceeb30d3fddeef8
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2017-03-18 19:21:26 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html
    
    Se creó el Login.php

---
+++ php/Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Iniciar Sesión</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>Bienvenido a Bellavista Dental</h1>
+    <h3 class="Textos">Iniciar Sesión</h3>
+
+    <form>
+        <input type="text" name="txtusuario" placeholder="Usuario"> <br>
+        <input type="password" name="txtcontra" placeholder="Contraseña">
+    </form>
+    <input type="submit" name="Enviar" value="Iniciar Sesión">
+    <h5>
+        ¿No tienes usuario?
+        <a target="_blank"> Registrate Aquí</a>
+    </h5>
+
+</body>
+</html>
