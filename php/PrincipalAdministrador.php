11f42c96e73857f6772918f928f4276838fd1477
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2017-03-19 01:12:56 -0700

    iconos para administrador

--- php/PrincipalAdministrador.php
+++ php/PrincipalAdministrador.php
@@ -3,16 +3,17 @@
 <head>
     <title>Bienvenido Administrador</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/menuVertical.css">
+    <link type="text/css" rel="stylesheet" href="../CSS/menuVerticalAdminitrador.css">
+    <link type="text/css" rel="stylesheet" href="../CSS/styleIcons.css">
 </head>
 <body>
 <h1>Opciones de administración</h1>
 <nav>
     <ul>
-        <li><a href="#"><span></span> Inicio </a></li>
+        <li class="home"><a href="#"><span></span> Inicio </a></li>
         <li> <a href="#"><span></span>Pacientes </a> </li>
         <li> <a href="#"><span></span>Recepcionistas </a></li>
-        <li> <a href="#"><span></span>Dentistas </a></li>
+        <li><a href="#"><span></span>Dentistas</a></li>
         <li> <a href="#"><span></span>Cerrar Sesión</a></li>
     </ul>
 </nav>
