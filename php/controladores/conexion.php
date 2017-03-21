d81f9f156dfdb8b6cc3a887f356ce3938cff9eb7
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2017-03-20 17:20:36 -0700

    conexion y control registra usuario paciente

---
+++ php/controladores/conexion.php
@@ -0,0 +1,20 @@
+<?php 
+$servidor= "localhost";
+$usuario="root";
+$contrasena="";
+$base="ProyectoDental";
+
+
+
+/*if(mysql_connect($servidor,$usuario,$contrasena))
+{
+	echo "conectado";
+	
+	if(mysql_select_db($base)){echo " BD seleccionada ";}
+} */
+// PRIMERO ES RECOMENDABLE REALIZAR PRUEBAS CON UN IF
+	mysql_connect($servidor,$usuario,$contrasena);
+	mysql_select_db($base);
+
+
+ ?>
