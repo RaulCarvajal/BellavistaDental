5b5ba700761bf955bf8f413f7b438ab1d0c1f9f7
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2017-03-20 18:13:31 -0700

    corregir txt de alta usuario

--- php/Signup.php
+++ php/Signup.php
@@ -15,28 +15,28 @@
         <div id="cont">
             <form class="w3-container" method="post" action="controladores/ControlRegistroUsuarioPaciente.php">
                 <label class="w3-label w3-text-blue"><b><h3>Nombre/s</h3></b></label>
-                <input name="txtNombre" class="w3-input w3-border w3-light-grey" type="text">
+                <input name="txtnombrepac" class="w3-input w3-border w3-light-grey" type="text">
 
                 <label class="w3-label w3-text-blue"><b><h3>Apellido</h3></b></label>
-                <input name="txtApellido" class="w3-input w3-border w3-light-grey" type="text">
+                <input name="txtapellido" class="w3-input w3-border w3-light-grey" type="text">
 
                 <label class="w3-label w3-text-blue"><b><h3>Nombre de usuario</h3></b></label>
-                <input name="txtNombreUsu" class="w3-input w3-border w3-light-grey" type="text">
+                <input name="txtuser" class="w3-input w3-border w3-light-grey" type="text">
 
                 <label class="w3-label w3-text-blue"><b><h3>Contraseña</h3></b></label>
-                <input name="txtContraseña" class="w3-input w3-border w3-light-grey" type="password">
+                <input name="txtcontrasena" class="w3-input w3-border w3-light-grey" type="password">
 
                 <label class="w3-label w3-text-blue"><b><h3>Correo electronico</h3></b></label>
-                <input name="txtCorreo" class="w3-input w3-border w3-light-grey" type="text">
+                <input name="txtcorreo" class="w3-input w3-border w3-light-grey" type="text">
 
                 <label class="w3-label w3-text-blue"><b><h3>Telefono(Con lada)</h3></b></label>
-                <input name="txtTelefono" class="w3-input w3-border w3-light-grey" type="text">
+                <input name="txttelefono" class="w3-input w3-border w3-light-grey" type="text">
 
                 <label class="w3-label w3-text-blue"><b><h3>Fecha de nacimiento</h3></b></label>
-                <input name="txtFechaNac" class="w3-input w3-border w3-light-grey" type="date">
+                <input name="txtfecnac" class="w3-input w3-border w3-light-grey" type="date">
 
                 <label class="w3-label w3-text-blue"><b><h3>Sexo</h3></b></label>
-                <select class="cmb">
+                <select class="cmb" name="sexo" >
                     <option value="null">Seleccionar uno</option>
                     <option value="Masculino">>Maculino</option>
                     <option value="Femenino">Femenino</option>
--- php/controladores/conexion.php
+++ php/controladores/conexion.php
@@ -4,14 +4,12 @@ $usuario="root";
 $contrasena="";
 $base="proyectodental";
 
-
-
 if(mysql_connect($servidor,$usuario,$contrasena))
 {
 	echo "conectado";
 	
 	if(mysql_select_db($base)){echo " BD seleccionada ";}
-}
+} 
 // PRIMERO ES RECOMENDABLE REALIZAR PRUEBAS CON UN IF
 	mysql_connect($servidor,$usuario,$contrasena);
 	mysql_select_db($base);
